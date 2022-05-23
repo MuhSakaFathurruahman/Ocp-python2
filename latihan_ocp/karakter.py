b7ced304e0350fe5db733e15d82072e6cb3574c3
MuhsakaFathurrahman <msakafathurrahman@gmail.com>
2022-05-23 23:07:35 +0700

    karakter_fathur

---
+++ latihan_ocp/karakter.py
@@ -0,0 +1,16 @@
+from abc import ABC, abstractmethod
+class Karakter(ABC):
+    
+    def __init__(self, nama: str, power: int):
+        self.__nama = nama
+        self.__power = power
+        
+    def get_nama(self) -> str:
+        return self.__nama
+    
+    def get_power(self) -> int:
+        return self.__power
+    
+    @abstractmethod
+    def menyerang(self) -> int:
+        pass
